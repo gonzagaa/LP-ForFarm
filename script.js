b317eafd7fdb825e7c9acfe39f78df1ff127da64
gonzagaa <iigust4vo@gmail.com>
2024-07-10 16:10:38 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+function onScroll() {
+    showNavOnScroll()
+}
+
+function showNavOnScroll() {
+    if(scrollY > 0) {
+        document.querySelector("#navigation").classList.add("scroll")
+    } else {
+        document.querySelector("#navigation").classList.remove("scroll")
+    }
+}
+
+function openMenu() {
+    document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+    document.body.classList.remove('menu-expanded')
+}
